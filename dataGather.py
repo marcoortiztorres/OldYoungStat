9862f64e9ca1a45667d8e5513ee238b8ba2dbf66
marcoortiztorres <maor9361@colorado.edu>
2019-04-23 14:08:18 -0600

    skeleton for data gathering

---
+++ dataGather.py
@@ -0,0 +1,21 @@
+from pybaseball.lahman import *
+import pandas as pd
+import matplotlib.pylab as plt
+%matplotlib inline
+
+class player:
+    playerID = ''
+    playerName = ''
+    palyerAge = 24
+
+    def getStatcast(self, playerID)
+        return dp.DataFrame()
+
+    def getName(self):
+        if self.playerName == '':
+            pass
+        else:
+            return self.playerName
+
+    def getLahman(self, playerID):
+        return dp.DataFrame()
